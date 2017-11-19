fac1fee097a0c6128bb93b212fde9df54d523710
Maverick Tesini <mavericktesini@gmail.com>
2017-11-19 07:31:12 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -34,7 +34,6 @@ class Question{
     return new Promise(function(resolve) {
       db.get(sql, [id], function(err, result){
         let question = new Question(result.content);
-        question.result = result.content;
         question.id = result.id;
         resolve(question);
       })
